e24fdd896a1ea00023862e81cbfc7aad3b1d2b05
Krishnajha25 <Krishnajha366@gmail.com>
2021-01-01 13:36:00 +0530

    Card1 completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Card1 from './components/Card1/Card1';
 // 0721cd6e70fa645c25ea7c923611d6e4
 function App() {
+  const img = "https://drop.ndtv.com/albums/COOKS/pasta-vegetarian/pastaveg_640x480.jpg"
+  const title="First title"
   return (
     <div className="App">
       <h1>Zomato-clone</h1>
+      <div className="cardContainer">
+        <Card1 img={img} title={title} />
+        <Card1 img={img} title={title} />
+        <Card1 img={img} title={title} />
+        <Card1 img={img} title={title} />
+      </div>
     </div>
   );
 }
